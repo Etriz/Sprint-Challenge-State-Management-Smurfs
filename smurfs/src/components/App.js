8f8a2066deda49ec1eb3ca71462d8e95f8702857
ryanp <ryanpanimation@gmail.com>
2020-05-29 20:41:02 -0500

    displays smurfs data

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import { SmurfContext } from "../contexts/SmurfContext";
+
 import AddSmurfForm from "./AddSmurfForm";
+import SmurfDisplay from "./SmurfDisplay";
 const App = () => {
   const [smurfData, setSmurfData] = useState([]);
   useEffect(() => {
@@ -15,13 +17,14 @@ const App = () => {
       .catch((err) => console.log(err));
   }, []);
   return (
-    <SmurfContext.Provider value={smurfData}>
+    <SmurfContext.Provider value={{ smurfData }}>
       <div className="App">
         <h1>SMURFS! 2.0 W/ Context</h1>
         {/* <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div> */}
         <AddSmurfForm />
+        <SmurfDisplay />
       </div>
     </SmurfContext.Provider>
   );
--- smurfs/src/components/SmurfDisplay.jsx
+++ smurfs/src/components/SmurfDisplay.jsx
@@ -1,7 +1,19 @@
 import React, { useContext } from "react";
+import { SmurfContext } from "../contexts/SmurfContext";
 
 const SmurfDisplay = () => {
-  return <div></div>;
+  const { smurfData } = useContext(SmurfContext);
+  return (
+    <div>
+      {smurfData.map((smurf) => (
+        <div>
+          <p>Name: {smurf.name}</p>
+          <p>Age: {smurf.age}</p>
+          <p>Height: {smurf.height}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SmurfDisplay;
